ff1f375836da903200a7eb198c8d526744e7bfa5
ceren.durmus <cerengul2000945@gmail.com>
2024-10-27 14:02:33 +0300

    fix

--- frontend/src/app/shared/layouts/main-layout/main-layout.component.ts
+++ frontend/src/app/shared/layouts/main-layout/main-layout.component.ts
@@ -1,11 +1,13 @@
+import { HeaderComponent } from './../../components/header/header.component';
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { NavbarComponent } from '../../components/navbar/navbar.component';
+
+
 
 @Component({
   selector: 'app-main-layout',
   standalone: true,
-  imports: [RouterOutlet, NavbarComponent],
+  imports: [RouterOutlet, HeaderComponent],
   templateUrl: './main-layout.component.html',
   styleUrl: './main-layout.component.scss',
 })
