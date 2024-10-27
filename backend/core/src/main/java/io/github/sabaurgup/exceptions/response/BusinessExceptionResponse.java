4411ac4f6ae3f1de53637c8999ea7da208270bc2
SabaUrgup <sabaurgup@gmail.com>
2024-10-27 14:02:26 +0300

    deploy exception rules

--- backend/core/src/main/java/io/github/sabaurgup/exceptions/response/BusinessExceptionResponse.java
+++ backend/core/src/main/java/io/github/sabaurgup/exceptions/response/BusinessExceptionResponse.java
@@ -1,14 +1,14 @@
 package io.github.sabaurgup.exceptions.response;
 
-import lombok.AllArgsConstructor;
 import lombok.Getter;
-import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 @Getter
 @Setter
-@NoArgsConstructor
-@AllArgsConstructor
 public class BusinessExceptionResponse {
     private String message;
+
+    public BusinessExceptionResponse(String message) {
+        this.message = message;
+    }
 }
