9e87f8d0988a9b312065c34ae3e67c87f387f5e6
ceren.durmus <cerengul2000945@gmail.com>
2024-10-28 15:53:48 +0300

    searchCustomer finish

--- frontend/src/app/features/customer/customer-search/customer-search.component.ts
+++ frontend/src/app/features/customer/customer-search/customer-search.component.ts
@@ -5,11 +5,12 @@ import { CustomerSearchRequest } from '../../../shared/models/customer/customerS
 import { CustomerSearchResponse } from '../../../shared/models/customer/customerSearchResponse';
 import { MainLayoutComponent } from '../../../shared/layouts/main-layout/main-layout.component';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-customer-search',
   standalone: true,
-  imports: [MainLayoutComponent, FormsModule, ReactiveFormsModule],
+  imports: [MainLayoutComponent, FormsModule, ReactiveFormsModule, CommonModule],
   templateUrl: './customer-search.component.html',
   styleUrls: ['./customer-search.component.scss']
 })
@@ -17,6 +18,11 @@ export class CustomerSearchComponent implements OnInit {
   searchForm: FormGroup;
   results: CustomerSearchResponse[] = [];
   isSearchEnabled = false;
+  searched = false;
+
+  page: number = 1;
+  pageSize: number = 10;
+  totalResults: number = 0;
 
   constructor(private fb: FormBuilder, private customerService: CustomerService) {
     this.searchForm = this.fb.group({
@@ -36,14 +42,22 @@ export class CustomerSearchComponent implements OnInit {
   ngOnInit(): void {}
 
   search(): void {
-    if (this.searchForm.valid) {
+    if (this.isSearchEnabled) {
       const searchRequest: CustomerSearchRequest = this.searchForm.value;
       this.customerService.searchCustomer(searchRequest).subscribe(
         (response) => {
           this.results = response;
+          this.totalResults = response.length;
+          this.searched = true; // Arama yapıldı
+          // Eğer sonuç yoksa, boş array döndüğünden emin olalım
+          if (this.results.length === 0) {
+            this.results = []; // Eğer sonuç boşsa yine de results dizisi sıfırlanmış olur
+          }
         },
         (error) => {
           console.error('Search failed', error);
+          this.results = [];
+          this.searched = true; // Arama yapıldı olarak işaretle
         }
       );
     }
@@ -53,5 +67,28 @@ export class CustomerSearchComponent implements OnInit {
     this.searchForm.reset();
     this.results = [];
     this.isSearchEnabled = false;
+    this.page = 1;
+    this.searched = false;
+  }
+
+  get paginatedResults() {
+    const start = (this.page - 1) * this.pageSize;
+    return this.results.slice(start, start + this.pageSize);
+  }
+
+  nextPage() {
+    if (this.page * this.pageSize < this.totalResults) {
+      this.page++;
+    }
+  }
+
+  previousPage() {
+    if (this.page > 1) {
+      this.page--;
+    }
+  }
+
+  get totalPages() {
+    return Math.ceil(this.totalResults / this.pageSize);
   }
 }
