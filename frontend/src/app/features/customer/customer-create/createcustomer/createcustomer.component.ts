86916a230135d607f769add444e92dcfe8d6949a
Eylül Aktuy <79935576+eylulaktuy@users.noreply.github.com>
2024-10-28 16:30:17 +0300

    create customer components

---
+++ frontend/src/app/features/customer/customer-create/createcustomer/createcustomer.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-createcustomer',
+  standalone: true,
+  imports: [],
+  templateUrl: './createcustomer.component.html',
+  styleUrl: './createcustomer.component.scss'
+})
+export class CreatecustomerComponent {
+
+}
---
+++ frontend/src/app/features/customer/customer-create/customerinfo/customerinfo.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-customerinfo',
+  standalone: true,
+  imports: [],
+  templateUrl: './customerinfo.component.html',
+  styleUrl: './customerinfo.component.scss'
+})
+export class CustomerinfoComponent {
+
+}
