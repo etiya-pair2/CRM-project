2f6ed023c9892b8e14bd11eaf20da24e83ca6a60
Eylül Aktuy <79935576+eylulaktuy@users.noreply.github.com>
2024-10-28 22:05:03 +0300

    create customer pages

--- frontend/src/app/features/customer/customer.module.ts
+++ frontend/src/app/features/customer/customer.module.ts
@@ -3,12 +3,14 @@ import { CommonModule } from '@angular/common';
 import { ReactiveFormsModule } from '@angular/forms';
 import { CustomerRoutingModule } from './customer-routing.module';
 import { CustomerSearchComponent } from './customer-search/customer-search.component';
+import { CreatecustomerComponent } from './customer-create/createcustomer/createcustomer.component';
 
 @NgModule({
-  imports: [
+imports: [
     CommonModule,
     CustomerRoutingModule,
     ReactiveFormsModule,
+    CreatecustomerComponent,
   ],
 })
 export class CustomerModule { }
