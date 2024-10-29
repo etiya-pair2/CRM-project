bd980393ef05da9fb7f9dd276e75839af0ba71ca
ceren.durmus <cerengul2000945@gmail.com>
2024-10-29 11:47:28 +0300

    fix core

--- backend/identityservice/src/main/java/com/etiya/identityservice/rules/AuthBusinessRules.java
+++ backend/identityservice/src/main/java/com/etiya/identityservice/rules/AuthBusinessRules.java
@@ -2,8 +2,6 @@ package com.etiya.identityservice.rules;
 
 import com.etiya.identityservice.entity.User;
 import com.etiya.identityservice.repository.UserRepository;
-import io.github.sabaurgup.exceptions.BusinessException;
-import io.github.sabaurgup.exceptions.Messages;
 import lombok.RequiredArgsConstructor;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Component;
@@ -21,7 +19,7 @@ public class AuthBusinessRules {
     public void checkIfEmailExist(String email) {
         Optional<User> userOptional = userRepository.findByEmailIgnoreCase(email);
         if (userOptional.isPresent()) {
-            throw new BusinessException(Messages.USER_EMAIL_ALREADY_EXISTS);
+//            throw new BusinessException(Messages.USER_EMAIL_ALREADY_EXISTS);
         }
     }
 
