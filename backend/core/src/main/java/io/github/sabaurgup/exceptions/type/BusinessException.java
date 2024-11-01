120a116048085cc84138ae3570603e8ee48b9cac
SabaUrgup <sabaurgup@gmail.com>
2024-11-01 16:13:23 +0300

    add @EnableCore

--- backend/core/src/main/java/io/github/sabaurgup/exceptions/type/BusinessException.java
+++ backend/core/src/main/java/io/github/sabaurgup/exceptions/type/BusinessException.java
@@ -1,10 +1,5 @@
 package io.github.sabaurgup.exceptions.type;
 
-import lombok.AllArgsConstructor;
-import lombok.Data;
-import lombok.Getter;
-
-
 public class BusinessException extends RuntimeException
 {
     public BusinessException(String message) {
--- backend/identityservice/src/main/java/com/etiya/identityservice/IdentityserviceApplication.java
+++ backend/identityservice/src/main/java/com/etiya/identityservice/IdentityserviceApplication.java
@@ -1,8 +1,10 @@
 package com.etiya.identityservice;
 
+import io.github.sabaurgup.annotations.EnableCore;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+@EnableCore
 @SpringBootApplication
 public class IdentityserviceApplication {
 
