97b97359b666fd50c90feb56c556bb1d6166f8ca
AlpcanYuksel <alpcanyuksel24@gmail.com>
2024-11-01 15:07:41 +0300

    WebMvcConfigurer

--- backend/gateway/src/main/java/com/etiya/gateway/configuration/CorsConfig.java
+++ backend/gateway/src/main/java/com/etiya/gateway/configuration/CorsConfig.java
@@ -1,9 +1,24 @@
 package com.etiya.gateway.configuration;
 
-import io.github.sabaurgup.webConfig.BaseWebConfiguration;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
-public class CorsConfig extends BaseWebConfiguration {
+public class CorsConfig{
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/api/**")
+                        .allowedOrigins("http://localhost:4200")
+                        .allowedMethods("*")
+                        .allowedHeaders("*")
+                        .allowCredentials(true);
+            }
+        };
+    }
 
 }
