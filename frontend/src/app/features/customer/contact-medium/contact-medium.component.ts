7275acd06b5e1753f258a5ad75a28bfaaf1ba35b
SabaUrgup <sabaurgup@gmail.com>
2024-11-02 11:41:18 +0300

    add FR9 contact medium info design

--- frontend/src/app/features/customer/contact-medium/contact-medium.component.ts
+++ frontend/src/app/features/customer/contact-medium/contact-medium.component.ts
@@ -1,6 +1,9 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { NavbarComponent } from '../../../shared/components/navbar/navbar.component';
 import { MainLayoutComponent } from '../../../shared/layouts/main-layout/main-layout.component';
+import { CustomerService } from '../../../shared/services/customer.service';
+import { contactMediumInfoResponse } from '../../../../app/shared/models/customer/contactMediumInfoResponse';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-contact-medium',
@@ -9,6 +12,35 @@ import { MainLayoutComponent } from '../../../shared/layouts/main-layout/main-la
   templateUrl: './contact-medium.component.html',
   styleUrl: './contact-medium.component.scss'
 })
-export class ContactMediumComponent {
+export class ContactMediumComponent implements OnInit {
+  contact: contactMediumInfoResponse | null = null;
 
+  constructor(
+    private route: ActivatedRoute,
+    private customerService: CustomerService) {}
+
+  ngOnInit() {
+    this.route.queryParams.subscribe(params => {
+      const customerId = params['customerId'];
+      if (customerId) {
+        this.fetchContactDetails(customerId);
+      }
+    });
+  }
+
+  fetchContactDetails(customerId: string) {
+    this.customerService.getContactDetails(customerId).subscribe(
+      (response: contactMediumInfoResponse) => {
+        this.contact = response;
+        console.log(response);
+      },
+      (error) => {
+        console.error('Error fetching contact medium details:', error);
+      }
+    );
+  }
+
+  onEdit(): void {
+    // Handle edit logic here
+  }
 }
--- frontend/src/app/features/customer/customer-create/create-contact-medium/createcustomer-contact-medium.component.ts
+++ frontend/src/app/features/customer/customer-create/create-contact-medium/createcustomer-contact-medium.component.ts
@@ -1,81 +1,81 @@
 import { MainLayoutComponent as MainLayoutComponent } from "../../../../shared/layouts/main-layout/main-layout.component";
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
-import { ActivatedRoute, Router, RouterModule } from '@angular/router';
+import { ActivatedRoute, Router,RouterModule } from '@angular/router';
 import { CustomerService } from '../../../../shared/services/customer.service';
 import { customerCreateContactMedRequest } from '../../../../shared/models/customer/customerCreateContactMedRequest';
-import { customerCreateContactMedResponse } from '../../../../shared/models/customer/customerCreateContactMedResponse';
+import { customerCreateContactMedResponse} from '../../../../shared/models/customer/customerCreateContactMedResponse';
 import { Component } from "@angular/core";
 
 @Component({
   selector: 'app-createcustomer-contact-medium',
   standalone: true,
-  imports: [MainLayoutComponent, CommonModule, FormsModule, RouterModule],
+  imports: [MainLayoutComponent,CommonModule,FormsModule,RouterModule],
   templateUrl: './createcustomer-contact-medium.component.html',
   styleUrl: './createcustomer-contact-medium.component.scss'
 })
 export class CreatecustomerContactMediumComponent {
-  email: string = '';
-  mobilePhone: string = '';
-  homePhone: string = '';
-  fax: string = '';
+  email:string= '';
+  mobilePhone:string='';
+  homePhone:string='';
+  fax:string='';
   isNextEnabled: boolean = false; // Butonun aktifliği
   showError: boolean = false;
-  mobilePhoneErrorMessage: string = '';
-  emailErrorMessage: string = '';
-  customerId?: string;
+  mobilePhoneErrorMessage: string='';
+  emailErrorMessage: string= '';
+  customerId?: string ;
+ 
+  constructor( private customerService: CustomerService, private router: Router, private route: ActivatedRoute) {};
 
-  constructor(private customerService: CustomerService, private router: Router, private route: ActivatedRoute) { };
-
-  ngOnInit(): void {
+  ngOnInit(): void {   
     this.route.queryParams.subscribe(params => {
-      this.customerId = params['customerId'];
+      this.customerId = params['customerId']; 
     });
-  }
+}
   onInputChange() {
     this.isNextEnabled = this.email.trim().length > 0 && this.mobilePhone.trim().length > 0; // Boş değilse aktif et
   }
 
-
+  
   next(): void {
-    this.showError = false;
-
-    if (!this.email) {
-      this.emailErrorMessage = 'E-mail cannot be empty.';
-      this.showError = true;
-    }
-
-    if (!this.mobilePhone) {
-      this.mobilePhoneErrorMessage = 'Mobile Phone cannot be empty.';
-      this.showError = true;
-    }
-
-    if (this.showError) {
-      return; // Hata varsa fonksiyondan çık
-    }
-
-
-    const customerCreateContactMedRequest: customerCreateContactMedRequest = {
-      email: this.email,
-      mobilePhone: this.mobilePhone,
-      homePhone: this.homePhone,
-      fax: this.fax,
-      customerId: this.customerId!
-    }// customerId'yi ekleyin};
-
-    this.customerService.createCustomerContactMedium(customerCreateContactMedRequest).subscribe(
-      (response: customerCreateContactMedResponse[]) => {
-        console.log('Customer contact medium created successfully:', response);
-        this.router.navigate(['/customer/generalInfo']);
-      },
-      (error: any) => {
-        console.error('Error occurred while creating customer contact medium:', error);
+      this.showError = false; 
+  
+      if (!this.email) {
+        this.emailErrorMessage = 'E-mail cannot be empty.';
+        this.showError = true;
       }
-    );
-
+      
+      if (!this.mobilePhone) {
+        this.mobilePhoneErrorMessage = 'Mobile Phone cannot be empty.';
+        this.showError = true;
+      }
+  
+      if (this.showError) {
+        return; 
+      }
+  
+      
+      const customerCreateContactMedRequest: customerCreateContactMedRequest = { 
+        email: this.email, 
+        mobilePhone: this.mobilePhone, 
+        homePhone: this.homePhone, 
+        fax: this.fax ,
+        customerId: this.customerId! 
+      }
+        
+      this.customerService.createCustomerContactMedium(customerCreateContactMedRequest).subscribe(
+        (response: customerCreateContactMedResponse[]) => {
+          console.log('Customer contact medium created successfully:', response);
+          this.router.navigate(['/customer/generalInfo'],{ queryParams: { customerId: this.customerId } });
+        },
+        (error: any) => {
+          console.error('Error occurred while creating customer contact medium:', error);
+        }
+      );
+    
   }
-
-
-
+  
+  
+  
 
 }
