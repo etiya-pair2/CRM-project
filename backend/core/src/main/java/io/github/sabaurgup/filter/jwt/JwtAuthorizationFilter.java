0895eb670796fdcd73e747cf4303b27c1abb3a4f
AlpcanYuksel <alpcanyuksel24@gmail.com>
2024-11-05 23:46:48 +0300

    jwt authorization filter

--- backend/core/src/main/java/io/github/sabaurgup/filter/jwt/JwtAuthorizationFilter.java
+++ backend/core/src/main/java/io/github/sabaurgup/filter/jwt/JwtAuthorizationFilter.java
@@ -1,61 +1,53 @@
-//package io.github.sabaurgup.filter.jwt;
-//
-//import io.github.sabaurgup.security.BaseJwtService;
-//import io.jsonwebtoken.Claims;
-//import jakarta.servlet.FilterChain;
-//import jakarta.servlet.ServletException;
-//import jakarta.servlet.http.HttpServletRequest;
-//import jakarta.servlet.http.HttpServletResponse;
-//import org.springframework.beans.factory.annotation.Autowired;
-//import org.springframework.http.HttpHeaders;
-//import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
-//import org.springframework.security.core.GrantedAuthority;
-//import org.springframework.security.core.authority.SimpleGrantedAuthority;
-//import org.springframework.security.core.context.SecurityContextHolder;
-//import org.springframework.security.web.authentication.WebAuthenticationDetailsSource;
-//import org.springframework.stereotype.Component;
-//import org.springframework.web.filter.OncePerRequestFilter;
-//
-//import java.io.IOException;
-//import java.util.ArrayList;
-//import java.util.List;
-//
-//@Component
-//public class JwtAuthorizationFilter extends OncePerRequestFilter {
-//
-//    @Autowired
-//    private BaseJwtService jwtService;
-//
-//    @Override
-//    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain)
-//            throws ServletException, IOException {
-//        String authHeader = request.getHeader(HttpHeaders.AUTHORIZATION);
-//        try {
-//            if (authHeader != null && authHeader.startsWith("Bearer ")) {
-//                // Autherization header'ı gönderilmiş ise ve "Bearer " ile başlıyorsa
-//                String token = authHeader.substring(7);
-//
-//                if (!"".equals(token)) {
-//                    // token gönderilmiş ise
-//                    Claims claims = jwtService.getClaimsFromToken(token);
-//                    String email = claims.getId();
-//                    @SuppressWarnings("unchecked")
-//                    ArrayList<String> roles = claims.get("roles", ArrayList.class);
-//                    List<GrantedAuthority> authorities = new ArrayList<GrantedAuthority>();
-//                    // String[] içindeki her bir rol için SimpleGrantedAuthority objesi oluşturup
-//                    // authorities listesine ekledik.
-//                    roles.forEach(r -> authorities.add(new SimpleGrantedAuthority("ROLE_" + r)));
-//
-//                    UsernamePasswordAuthenticationToken authentication = new UsernamePasswordAuthenticationToken(email,
-//                            "", authorities);
-//                    authentication.setDetails(new WebAuthenticationDetailsSource().buildDetails(request));
-//                    SecurityContextHolder.getContext().setAuthentication(authentication);
-//                }
-//            }
-//        } catch (Exception ex) {
-//            // jwt token kontrolü sırasında hata oluştuysa bir şey yapma
-//        }
-//        filterChain.doFilter(request, response);
-//    }
-//
-//}
+package io.github.sabaurgup.filter.jwt;
+
+import io.github.sabaurgup.security.BaseJwtService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.web.authentication.WebAuthenticationDetailsSource;
+import org.springframework.stereotype.Component;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+import java.io.IOException;
+
+@Component
+@RequiredArgsConstructor
+public class JwtAuthorizationFilter extends OncePerRequestFilter {
+
+    private final BaseJwtService jwtService;
+
+
+    @Override
+    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain)
+            throws ServletException, IOException {
+
+        String authorizationHeader = request.getHeader("Authorization");
+
+        String token = null;
+        String username = null;
+
+        if (authorizationHeader != null && authorizationHeader.startsWith("Bearer ")) {
+            token = authorizationHeader.substring(7);
+            username = jwtService.extractUsername(token);
+        }
+
+        if (username != null && SecurityContextHolder.getContext().getAuthentication() == null) {
+
+            if (jwtService.validateToken(token)) {
+                UsernamePasswordAuthenticationToken authToken =
+                        new UsernamePasswordAuthenticationToken(username, null, null);
+                authToken.setDetails(new WebAuthenticationDetailsSource().buildDetails(request));
+
+                SecurityContextHolder.getContext().setAuthentication(authToken);
+            }
+        }
+
+        filterChain.doFilter(request, response);
+    }
+}
+
--- backend/core/src/main/java/io/github/sabaurgup/security/BaseSecurityConfiguration.java
+++ backend/core/src/main/java/io/github/sabaurgup/security/BaseSecurityConfiguration.java
@@ -1,40 +1,44 @@
 package io.github.sabaurgup.security;
 
+import io.github.sabaurgup.filter.jwt.JwtAuthorizationFilter;
+import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
-import org.springframework.web.servlet.config.annotation.CorsRegistry;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 @EnableWebSecurity
 @Configuration
+@RequiredArgsConstructor
 public class BaseSecurityConfiguration {
+
+    private final JwtAuthorizationFilter jwtFilter;
+
     @Bean
     public PasswordEncoder passwordEncoder() {
         return new BCryptPasswordEncoder();
     }
 
     @Bean
-    public BaseJwtService baseJwtService() {
-        return new BaseJwtService();
-    }
-
-    @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http
-
                 .csrf(AbstractHttpConfigurer::disable)
                 .httpBasic(AbstractHttpConfigurer::disable)
-                .authorizeHttpRequests(req -> req.anyRequest().permitAll());
-        //.addFilterBefore(jwtFilter, UsernamePasswordAuthenticationFilter.class);
+                .authorizeHttpRequests(auth -> auth
+                        // Swagger ve OpenAPI dokümantasyon URL'leri için izin ver
+                        .requestMatchers("/swagger-ui/**", "/v3/api-docs/**", "/api/v1/identity/auth/login","/api/v1/identity/auth/register").permitAll()
+                        // Diğer tüm isteklerde kimlik doğrulaması iste
+                        .anyRequest().authenticated()
+                )
+                .sessionManagement(sess -> sess.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+                .addFilterBefore(jwtFilter, UsernamePasswordAuthenticationFilter.class); // JWT filtresini ekle
+
         return http.build();
     }
-
-
-
 }
